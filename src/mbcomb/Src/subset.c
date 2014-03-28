672dc50b452b4e8b911f1009da43f6ffafeaff8e
Randy Lai <randy.cs.lai@gmail.com>
2014-03-28 15:55:22 -0700

    bugfix for subset.c by Martin

--- src/mbcomb/Src/subset.c
+++ src/mbcomb/Src/subset.c
@@ -8,38 +8,27 @@
 
 unsigned int MBnext_subset(unsigned int *ar, size_t n)
 {
-	/* Find the first 1 with a 0 on its right, or the number of 1s */
+	unsigned int finished = 0;
+	unsigned int changed = 0;
 	unsigned int i;
-	unsigned int found = 0, finished = 0, ones = 0;
-	unsigned int result = 1;
 
-	for (i = n - 2; !found && !finished; i--) {
-		found = ar[i] == 1 && ar[i + 1] == 0;
-		finished = i == 0;
-		ones += ar[i + 1] == 1;
-	}
-	if (found) {
-		/* Move the 1 right */
-		ar[i + 1] = 0;
-		ar[i + 2] = 1;
-	}
-	else  if (ar[i + 1] == 0) {
-		/* Add a new 1 and place them all at the beginning */
-		for (i = 0; i < n; i++) {
-			if (i < ones + 1) {
-				ar[i] = 1;
-			}
-			else {
-				ar[i] = 0;
-			}
+	for (i = n - 1; !finished && !changed; i--) {
+		if (ar[i] == 0) {
+			/* Increment */
+			ar[i]++;
+			changed = 1;
 		}
+		else {
+			/* Roll over */
+			ar[i] = 0;
+		}
+		finished = i == 0;
 	}
-	else {
-		/* Finished */
+	if (!changed) {
+		/* Reset to first combination */
 		for (i = 0; i < n; i++) {
 			ar[i] = 0;
 		}
-		result = 0;
 	}
-	return result;
+	return changed;
 }
