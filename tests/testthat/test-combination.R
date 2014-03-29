d58ec485dd5ce7409775e8ded6e7a42ee51a1b01
Randy Lai <randy.cs.lai@gmail.com>
2014-03-28 18:19:08 -0700

    update tests

--- tests/testthat/test-combination.R
+++ tests/testthat/test-combination.R
@@ -3,19 +3,31 @@ getnext(I)
 expect_that(getnext(I)[3], equals(4))
 expect_that(nrow(getall(I)), equals(choose(5,3)))
 
-I = iterpc(c("a","b","c"), 3, ordered=FALSE)
+I = iterpc(3, 3, labels=c("a","b","c"), ordered=FALSE)
 getnext(I)
 expect_that(getnext(I)[3], equals(NULL))
 
-I = iterpc(c("a","b","c"), 2, ordered=FALSE)
+I = iterpc(3, 2, ordered=FALSE)
 expect_that(nrow(getall(I)), equals(choose(3,2)))
 
-I = iterpc(c("a","a","c","c"), 2, ordered=FALSE)
+I = iterpc(c(2,2), 2, labels=c("a", "c"), ordered=FALSE)
 getnext(I)
 expect_that(getnext(I)[2], equals("c"))
 expect_that(nrow(getall(I)), equals(3))
 
-I = iterpc(5, 3, 2, ordered=FALSE,replace=TRUE)
+I = iterpc(5, 3, ordered=FALSE,replace=TRUE)
 getnext(I)
 expect_that(getnext(I), equals(c(1,1,2)))
 expect_that(nrow(getall(I)), equals(35))
+
+I = iterpc(5, 5, ordered=FALSE)
+expect_that(nrow(getall(I)), equals(1))
+
+I = iterpc(5, 1, ordered=FALSE)
+expect_that(nrow(getall(I)), equals(5))
+
+I = iterpc(5, 5, labels=1:5, ordered=FALSE)
+expect_that(nrow(getall(I)), equals(1))
+
+I = iterpc(5, 1, labels=1:5, ordered=FALSE)
+expect_that(nrow(getall(I)), equals(5))
--- tests/testthat/test-permutation.R
+++ tests/testthat/test-permutation.R
@@ -2,18 +2,24 @@ I = iterpc(5, ordered=TRUE)
 expect_that(getnext(I)[3], equals(3))
 expect_that(nrow(getall(I)), equals(factorial(5)))
 
-I = iterpc(c("a","b","c"), ordered=TRUE)
+I = iterpc(3, labels=c("a","b","c"), ordered=TRUE)
 getnext(I)
 expect_that(getnext(I), equals(c("a","c","b")))
 
-I = iterpc(c("a","a","c"), ordered=TRUE)
+I = iterpc(c(2,1), labels=c("a","c"), ordered=TRUE)
 expect_that(nrow(getall(I)), equals(3))
 
-I = iterpc(c("a","a","c","c"), 2, ordered=TRUE)
+I = iterpc(c(2,2), 2, labels=c("a","c"), ordered=TRUE)
 expect_that(nrow(getall(I)), equals(4))
 getnext(I)
 expect_that(getnext(I)[2], equals("c"))
 
-I = iterpc(c("c","a","c","a"), 2, replace=TRUE, ordered=TRUE)
+I = iterpc(c(2,2), 2, labels=c("a","c"), replace=TRUE, ordered=TRUE)
 expect_that(nrow(getall(I)), equals(4))
 expect_that(getnext(I,2)[2,2], equals("c"))
+
+I = iterpc(5, 1, ordered=TRUE)
+expect_that(nrow(getall(I)), equals(5))
+
+I = iterpc(5, 1, labels=1:5, ordered=TRUE)
+expect_that(nrow(getall(I)), equals(5))
