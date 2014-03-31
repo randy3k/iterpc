bfb32470f86f31f47f22b1feb4460d82fc09fea2
Randy Lai <randy.cs.lai@gmail.com>
2014-03-30 20:10:17 -0700

    automatic deploy documentation by staticdocs

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# deploy to the gh-pages branch of the same repo
+
+./travis-tool.sh install_github hadley/staticdocs
+R -e "staticdocs::build_site()"
+cd ..
+if [ -z `git ls-remote --heads https://github.com/randy3k/iterpc | grep gh-pages` ]
+then
+    mkdir gh-pages
+    cd gh-pages
+    git init
+    git checkout -b gh-pages
+    git remote add origin https://${GH_TOKEN}@github.com/randy3k/iterpc.git
+else
+    git clone --branch gh-pages https://${GH_TOKEN}@github.com/randy3k/iterpc.git gh-pages
+    cd gh-pages
+fi
+echo "copy webpages from to here"
+cp -Rf ../iterpc/inst/web/ .
+git config --global user.email "randy.cs.lai@gmail.com"
+git config --global user.name "Randy Lai"
+git add -A :/
+git commit -m "Deploy site at $(date)"
+echo "deploy sites"
+git push -u origin gh-pages:gh-pages
