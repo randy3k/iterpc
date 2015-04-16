32a8332b0b2f72827125cd5f800b9f541a1f54ad
Randy Lai <randy.cs.lai@gmail.com>
2015-04-16 00:35:39 -0700

    bump to 0.2.7

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,106 +9,82 @@ using namespace Rcpp;
 SEXP next_combinations(IntegerVector x, unsigned int n, unsigned long d, IntegerVector status);
 RcppExport SEXP iterpc_next_combinations(SEXP xSEXP, SEXP nSEXP, SEXP dSEXP, SEXP statusSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP );
-        Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP );
-        SEXP __result = next_combinations(x, n, d, status);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP);
+    __result = Rcpp::wrap(next_combinations(x, n, d, status));
+    return __result;
 END_RCPP
 }
 // next_multiset_combinations
 SEXP next_multiset_combinations(IntegerVector multiset, IntegerVector x, unsigned long d, IntegerVector status);
 RcppExport SEXP iterpc_next_multiset_combinations(SEXP multisetSEXP, SEXP xSEXP, SEXP dSEXP, SEXP statusSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< IntegerVector >::type multiset(multisetSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP );
-        SEXP __result = next_multiset_combinations(multiset, x, d, status);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type multiset(multisetSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP);
+    __result = Rcpp::wrap(next_multiset_combinations(multiset, x, d, status));
+    return __result;
 END_RCPP
 }
 // next_combinations_replace
 SEXP next_combinations_replace(IntegerVector x, unsigned int n, unsigned long d, IntegerVector status);
 RcppExport SEXP iterpc_next_combinations_replace(SEXP xSEXP, SEXP nSEXP, SEXP dSEXP, SEXP statusSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP );
-        Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP );
-        SEXP __result = next_combinations_replace(x, n, d, status);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP);
+    __result = Rcpp::wrap(next_combinations_replace(x, n, d, status));
+    return __result;
 END_RCPP
 }
 // next_permutations
 SEXP next_permutations(IntegerVector x, unsigned long d, IntegerVector status);
 RcppExport SEXP iterpc_next_permutations(SEXP xSEXP, SEXP dSEXP, SEXP statusSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP );
-        SEXP __result = next_permutations(x, d, status);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP);
+    __result = Rcpp::wrap(next_permutations(x, d, status));
+    return __result;
 END_RCPP
 }
 // next_k_permutations
 SEXP next_k_permutations(IntegerVector x, unsigned int r, unsigned long d, IntegerVector status);
 RcppExport SEXP iterpc_next_k_permutations(SEXP xSEXP, SEXP rSEXP, SEXP dSEXP, SEXP statusSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< unsigned int >::type r(rSEXP );
-        Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP );
-        SEXP __result = next_k_permutations(x, r, d, status);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type r(rSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP);
+    __result = Rcpp::wrap(next_k_permutations(x, r, d, status));
+    return __result;
 END_RCPP
 }
 // next_permutations_replace
 SEXP next_permutations_replace(IntegerVector x, unsigned int n, unsigned long d, IntegerVector status);
 RcppExport SEXP iterpc_next_permutations_replace(SEXP xSEXP, SEXP nSEXP, SEXP dSEXP, SEXP statusSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP );
-        Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP );
-        Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP );
-        SEXP __result = next_permutations_replace(x, n, d, status);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type n(nSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type d(dSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type status(statusSEXP);
+    __result = Rcpp::wrap(next_permutations_replace(x, n, d, status));
+    return __result;
 END_RCPP
 }
