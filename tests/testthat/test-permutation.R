f0983d88db8e8d9bcfce153b56b0b0784bfdac72
Randy Lai <randy.cs.lai@gmail.com>
2016-04-08 16:56:45 -0400

    improve tests coding style

--- tests/testthat/test-permutation.R
+++ tests/testthat/test-permutation.R
@@ -1,31 +1,34 @@
 context("Permutation")
 
 test_that("Permutation", {
-    I <- iterpc(5, ordered=TRUE)
+    I <- iterpc(5, ordered = TRUE)
     expect_that(getnext(I)[3], equals(3))
     expect_that(getcurrent(I)[3], equals(3))
-    expect_that(nrow(getall(I)), equals(getlength(I)))
+    expect_that(nrow(getall(I)), equals(120))
 
-    I <- iterpc(3, labels=c("a","b","c"), ordered=TRUE)
+    I <- iterpc(3, labels = c("a", "b", "c"), ordered = TRUE)
     getnext(I)
-    expect_that(getnext(I), equals(c("a","c","b")))
-    expect_that(getcurrent(I), equals(c("a","c","b")))
+    expect_that(getnext(I), equals(c("a", "c", "b")))
+    expect_that(getcurrent(I), equals(c("a", "c", "b")))
 
-    I <- iterpc(c(2,1), labels=c("a","c"), ordered=TRUE)
-    expect_that(nrow(getall(I)), equals(getlength(I)))
+    I <- iterpc(c(2, 1), labels = c("a", "c"), ordered = TRUE)
+    expect_that(nrow(getall(I)), equals(3))
 
-    I <- iterpc(c(2,2), 2, labels=c("a","c"), ordered=TRUE)
-    expect_that(nrow(getall(I)), equals(getlength(I)))
+    I <- iterpc(c(2, 2), 2, labels = c("a", "c"), ordered = TRUE)
+    expect_that(nrow(getall(I)), equals(4))
     getnext(I)
     expect_that(getnext(I)[2], equals("c"))
 
-    I <- iterpc(c(2,2), 2, labels=c("a","c"), replace=TRUE, ordered=TRUE)
-    expect_that(nrow(getall(I)), equals(getlength(I)))
-    expect_that(getnext(I,2)[2,2], equals("c"))
+    I <- iterpc(c(2, 2), 2, labels = c("a", "c"), replace = TRUE, ordered = TRUE)
+    expect_that(nrow(getall(I)), equals(4))
+    expect_that(getnext(I, 2)[2, 2], equals("c"))
 
-    I <- iterpc(5, 1, ordered=TRUE)
-    expect_that(nrow(getall(I)), equals(getlength(I)))
+    I <- iterpc(5, 1, ordered = TRUE)
+    expect_that(nrow(getall(I)), equals(5))
 
-    I <- iterpc(5, 1, labels=1:5, ordered=TRUE)
-    expect_that(nrow(getall(I)), equals(getlength(I)))
+    I <- iterpc(5, 1, labels = 1:5, ordered = TRUE)
+    expect_that(nrow(getall(I)), equals(5))
+
+    I <- iterpc(5, 2, labels = 1:5, ordered = TRUE)
+    expect_that(nrow(getall(I)), equals(20))
 })
